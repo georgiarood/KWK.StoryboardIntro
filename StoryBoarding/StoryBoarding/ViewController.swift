d354483b69cc4ec86f6ea3a4f81ad4a5a4960cc9
georgiarood <51422481+georgiarood@users.noreply.github.com>
2020-08-05 11:33:36 -0400

    my first Xcode project!

---
+++ StoryBoarding/StoryBoarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoarding
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Georgia Rood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //this is a print statement, testing-testing
+    }
+
+
+}
+
